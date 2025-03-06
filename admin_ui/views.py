c4c5d37e6a4e55b93cc92df2f8c8b767e14b740e
mid9nytecrane <sheriffsakara41@gmail.com>
2025-03-06 00:19:48 +0000

    main commit

--- admin_ui/views.py
+++ admin_ui/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from accounts.decorators import admin_only,allowed_users
+from user_ui.models import TypeApproval
 # Create your views here.
 
 @admin_only
 def admin_ui(request):
-
-    return render(request, 'admin_ui/admin_ui.html')
+    type_approvals = TypeApproval.objects.all()
+    type_approvals_count = type_approvals.count()
+    return render(request, 'admin_ui/admin_ui.html', {'type_approvals_count':type_approvals_count})
 
 @admin_only
 def dashboard(request):
