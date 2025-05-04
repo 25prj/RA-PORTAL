fa1bd19bbf7cb31a8edb51f63c6bcbbb3cda275b
mid9nytecrane <sheriffsakara41@gmail.com>
2025-05-04 13:05:26 +0000

    change DEBUG=TRUE in settings.py

--- RADJANGO/settings.py
+++ RADJANGO/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)4t6x9!!5==5p#jh+wy*&zkl=v0+2(c+za6&j5(^o5w4bvlqw1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ra-portal-9.onrender.com']
 
