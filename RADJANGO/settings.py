7aca9c038693ade25ced73e729c9294fdf921cd7
mid9nytecrane <sheriffsakara41@gmail.com>
2025-05-04 10:33:59 +0000

    added crsf-trusted-origins to settings.py

--- RADJANGO/settings.py
+++ RADJANGO/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)4t6x9!!5==5p#jh+wy*&zkl=v0+2(c+za6&j5(^o5w4bvlqw1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ra-portal-9.onrender.com']
+
+CSRF_TRUSTED_ORIGINS = ['https://ra-portal-9.onrender.com']
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
